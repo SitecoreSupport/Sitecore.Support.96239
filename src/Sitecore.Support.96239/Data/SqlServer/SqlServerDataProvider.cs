fc939130643878ee580179c8afbf46e703fb510c
Chee How Yap <yho@sitecore.net>
2019-04-19 13:47:03 +0800

    Commit Patch

---
+++ src/Sitecore.Support.96239/Data/SqlServer/SqlServerDataProvider.cs
@@ -0,0 +1,27 @@
+﻿namespace Sitecore.Support.Data.SqlServer
+{
+  using Sitecore.Data.DataProviders.Sql;
+  using Sitecore.Data.SqlServer;
+  public class SqlServerDataProvider : Sitecore.Data.SqlServer.SqlServerDataProvider
+  {
+    private readonly SqlDataApi api;
+
+    public new SqlDataApi Api
+    {
+      get
+      {
+        return this.api;
+      }
+    }
+
+    public SqlServerDataProvider(string connectionString) : base(connectionString)
+    {
+      this.api = new SqlServerDataApi(connectionString);
+    }
+
+    protected override SqlDataProvider.FieldSharingConverter GetFieldSharingConverter()
+    {
+      return new Sitecore.Support.Data.DataProviders.Sql.FieldSharingConverter(this);
+    }
+  }
+}
